bdbde2b01552812ef914eef9eef82c12ede860b0
Alexander Damov <alexsander.damov@yahoo.com>
2024-08-05 20:45:47 +0300

    Add file path for current buffer in status line. Add keymaps for window resizing. Remove comments from lspconfig

--- lua/alex/plugins/colorscheme.lua
+++ lua/alex/plugins/colorscheme.lua
@@ -1,8 +1,27 @@
 return {
-
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function ()
-    vim.cmd("colorscheme tokyonight")
-  end
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		-- 	config = function()
+		-- 		vim.cmd("colorscheme tokyonight")
+		-- 	end,
+	},
+	{
+		-- "mcchrish/vim-no-color-collections", -- monochrome colorscheme
+		"andreasvc/vim-256noir",
+		-- config = function()
+		-- 	vim.cmd("colorscheme 256_noir")
+		-- end,
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+		config = function()
+			require("onedarkpro").setup({
+				options = {
+					transparency = true,
+				},
+			})
+			vim.cmd("colorscheme onedark")
+		end,
+	},
 }
--- lua/alex/plugins/lsp/omnisharp.lua
+++ lua/alex/plugins/lsp/omnisharp.lua
@@ -1,6 +1,5 @@
 return {
 	require("lspconfig").omnisharp.setup({
-
 		cmd = { "dotnet", "/home/jesper/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
 		on_attach = function(client, bufnr)
 			vim.api.nvim_create_autocmd("BufWritePost", {
